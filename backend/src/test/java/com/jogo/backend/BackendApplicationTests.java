df25b4b4fd2046b8a97d1861f9ebe5d9546534a7
Nathi <nathiellygames@gmail.com>
2026-02-09 20:13:02 -0300

    Add files via upload

---
+++ backend/src/test/java/com/jogo/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jogo.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
